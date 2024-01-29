b8f93dbe1b857ad9ebfca6af188a77f514b857ef
cpp-wizardry <153819258+cpp-wizardry@users.noreply.github.com>
2024-01-29 19:47:03 +0100

    Ajoutez des fichiers projet.

---
+++ Opengl game/Main.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <GL/GL.h>
+#include <GL/glew.h>
+#include <GL/GLU.h>
+#include <GL/wglew.h>
+#include <GL/glxew.h>
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+#include <glm/common.hpp>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
